6463c5c89b6aed9dfb14f8eb4255c5821d905fcb
Otobong Emmanuel <otobongemmanuelsn@gmail.com>
2026-06-26 14:52:23 +0100

    Fix RDS security group rules to allow EKS worker node access

--- terraform/eks/default/backend.tf
+++ terraform/eks/default/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project-bedrock-tfstate-altsoe0254655"
+    bucket = "project-bedrock-tfstate-650679031703"
     key    = "eks/default/terraform.tfstate"
     region = "us-east-1"
   }
